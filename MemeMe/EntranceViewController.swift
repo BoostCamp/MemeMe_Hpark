8773d1ce08a0b9e44e6b400385effeb200731997
HyunsooPark <qkrgustnrk@gmail.com>
2017-01-17 17:16:00 +0900

    Facebook Authentication With Firebase Successfully Done

--- MemeMe/EntranceViewController.swift
+++ MemeMe/EntranceViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+
 import Firebase
 import FirebaseInstanceID
 import FirebaseMessaging
+
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class EntranceViewController: UIViewController {
 
@@ -17,6 +21,35 @@ class EntranceViewController: UIViewController {
         super.viewDidLoad()
         FIRMessaging.messaging().subscribe(toTopic: "/topics/news")
     }
+    
+    // Open Facebook Login Manager in order to get credential from it
+    @IBAction func loginWithFacebookButtonTapped(_ sender: Any) {
+        let facebookLoginManager = FBSDKLoginManager()
+        facebookLoginManager.logIn(withReadPermissions: ["email", "public_profile"], from: self) { (result, error) in
+            if error != nil {
+                print(":::[HPARK] Unable to authenticate with Facebook \(error):::\n")
+            } else if result?.isCancelled == true {
+                print(":::[HPARK] App User cancelled Facebook Authentication :::\n")
+            } else {
+                print(":::[HPARK] Authentication with Facebook Successful :::\n")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    // After getting Facebook credential, sign in MemeMe App with the credential got from facebook login manager
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print(":::[HPARK] Unable to authenticate with Firebase - \(error) :::\n")
+            } else {
+                print(":::[HPARK] Successfully authenticated with Firebase :::\n")
+                // TODO: Segue to Main Page
+            }
+        })
+    }
+
 
 
 }
